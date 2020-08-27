ca62db1067f0e02150a46a253e4f94403d597ea7
Michał Kokosiński <19owen91@gmail.com>
2020-08-27 22:27:30 +0200

    reset gh-pages

---
+++ build/precache-manifest.07461b654e193a56930dae03f3b589c6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cba966898f394d1f2819e116510f774",
+    "url": "/e-widencja/index.html"
+  },
+  {
+    "revision": "ed57d2b32b1afed6b717",
+    "url": "/e-widencja/static/css/2.25ff2e5b.chunk.css"
+  },
+  {
+    "revision": "fbf8ddce7bf0b1c29176",
+    "url": "/e-widencja/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "ed57d2b32b1afed6b717",
+    "url": "/e-widencja/static/js/2.22d79923.chunk.js"
+  },
+  {
+    "revision": "e0546182649f50a2eb923c2a881ed896",
+    "url": "/e-widencja/static/js/2.22d79923.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbf8ddce7bf0b1c29176",
+    "url": "/e-widencja/static/js/main.86e85acb.chunk.js"
+  },
+  {
+    "revision": "4496e344ab59c56b3fb3",
+    "url": "/e-widencja/static/js/runtime-main.9fc514ea.js"
+  },
+  {
+    "revision": "6311196a304e3eba943efb867d21dc6a",
+    "url": "/e-widencja/static/media/itemDecorator.6311196a.svg"
+  }
+]);
